5fbd33715e7ef66616a16c8bcce0778a5a228684
Dillon Rowan <rowan.dillon@heb.com>
2023-08-17 19:55:46 -0500

    add navbar to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Navbar from '../components/Navbar'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      <body className={inter.className}>
+      <div className="title-content">
+        <h1 className="home-title mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Jennifer & Dillon</h1>
+      </div>
+      <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -6,33 +6,6 @@ export default async function Home() {
 
   return (
     <>
-    <div class={styles.titleContent}>
-      <h1 class="home-title mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Dillon & Jennifer</h1>
-      <nav className="flex sm:justify-center space-x-4">
-      {[
-        ['Home', '/'],
-        ['Our Story', '/story'],
-        ['Photos', '/photos'],
-        ['Q + A', '/qa'],
-        ['Travel', '/travel'],
-        ['Things to Do', '/activities'],
-        ['Registry', '/registry'],
-        ['RSVP', '/rsvp']
-      ].map(([title, url]) => (
-        <a href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
-      ))}
-    </nav>
-    </div>
-    
-
-
-      <div><Link href="/activities">Activities</Link></div>
-      <div><Link href="/photos">Photos</Link></div>
-      <div><Link href="/qa">QA</Link></div>
-      <div><Link href="/registry">Registry</Link></div>
-      <div><Link href="/rsvp">Rsvp</Link></div>
-      <div><Link href="/story">Story</Link></div>
-      <div><Link href="/travel">Travel</Link></div>
     </>
   )
 }
