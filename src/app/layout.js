598262d191448184a7810275dbca81dfc87b95a8
Dillon Rowan <rowan.dillon@heb.com>
2024-01-13 15:47:38 -0600

    update style of navbar, add sticky and background color

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,21 +32,36 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" className={`${cormorant.variable} h-full`} >
             <body >
-                <div className="grid grid-cols-[minmax(40px,_0.08fr)_minmax(0px,_1fr)_minmax(40px,_0.08fr)]">
-                    <Drawer navItems={navItems}/>
-                    <h1 className="font-cormorant select-none justify-self-center self-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
-                        Jennifer & Dillon
-                    </h1>
-                </div>                
+
+                {/* Header for non-mobile */}
+                <div className="hidden lg:block">
+                    <div className="grid grid-cols-[minmax(40px,_0.08fr)_minmax(0px,_1fr)_minmax(40px,_0.08fr)]">
+                        <div></div>
+                        <h1 className="mb-5 font-cormorant select-none justify-self-center self-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+                            Jennifer & Dillon
+                        </h1>
+                        <div></div>
+                    </div>    
+                    <Navbar navItems={navItems}/>
+                </div>  
+
+                {/* Header for mobile */}
+                <div className="lg:hidden">
+                    <div className="grid grid-cols-[minmax(40px,_0.08fr)_minmax(0px,_1fr)_minmax(40px,_0.08fr)] fixed bg-white z-10 w-full h-14 items-center">
+                        <Drawer navItems={navItems}/>
+                        <h1 className="font-cormorant select-none justify-self-center self-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+                            J & D
+                        </h1>
+                    </div>    
+                </div>                        
                 
-                <Navbar navItems={navItems}/>
-                <div className="py-5">{children}</div>
+                <div className="pt-12 pb-5">{children}</div>
                 <Image
                     className="rotate-180 podbar -z-10"
                     src="/leaves.webp"
                     width={2000}
                     height={1000}
-                    alt="Picture of the author"
+                    alt=""
                 />
             </body>
         </html>
