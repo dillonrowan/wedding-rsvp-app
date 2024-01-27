b03fa1316a750b0d707f26188818237286861bac
Dillon Rowan <rowan.dillon@heb.com>
2024-01-27 15:00:35 -0600

    add description to travel page

--- src/app/travel/page.js
+++ src/app/travel/page.js
@@ -7,6 +7,7 @@ export default function Travel() {
             <HotelCard name="Comfort Suites Buda - Austin South" address="15295 S I-35 Frontage Rd, Buda, TX 78610" phoneNumber="(512) 295-8600" website="https://www.choicehotels.com/texas/buda/comfort-suites-hotels/txc51?mc=llgoxxpx" />
             <HotelCard name="Fairfield Inn & Suites by Marriott Austin Buda" address="1240 Cabelas Dr, Buda, TX 78610" phoneNumber="(512) 295-1600" website="https://www.marriott.com/en-us/hotels/ausbu-fairfield-inn-and-suites-austin-buda/overview/?scid=f2ae0541-1279-4f24-b197-a979c79310b0" />
             <HotelCard name="Best Western Plus Buda Austin Inn & Suites" address="15295 S I-35 Frontage Rd Bldg 900, Buda, TX 78610" phoneNumber="(512) 361-0455" website="https://www.bestwestern.com/en_US/book/hotel-rooms.44737.html?iata=00171880&ssob=BLBWI0004G&cid=BLBWI0004G:google:gmb:44737" />
+            <p className="text-stone-500 lg:w-1/2">These are a few hotels closest to the venue. If you need help reaching the location, please do not hesitate to reach out to us or The Hummingbird House at (512) 934-0201. Their staff will be more than happy to assist. We look forward to seeing you at our wedding!</p>
         </div>
         
     );
