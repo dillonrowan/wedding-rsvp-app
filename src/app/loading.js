0505e52199c43b17dfe12fa0ce90914d1ffda6a9
Dillon Rowan <rowan.dillon@heb.com>
2024-02-04 13:18:00 -0600

    add component for floor image, fix bug with navigation drawer, fix warning about using class isntead of classname

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,3 +1,3 @@
 export default function Loading() {
-    return <div class="loader"></div>;
+    return <div className="loader"></div>;
 }
--- src/app/rsvp/search/loading.js
+++ src/app/rsvp/search/loading.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Loading() {
-    return <div class="loader"></div>;
+    return <div className="loader"></div>;
 }
