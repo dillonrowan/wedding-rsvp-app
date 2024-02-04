f1197b5c82d1d4303b41d8e09c01467c49ac4391
Dillon Rowan <rowan.dillon@heb.com>
2024-02-04 12:50:53 -0600

    add registry link to registry page, update style in rsvp page

--- src/app/registry/page.js
+++ src/app/registry/page.js
@@ -1,9 +1,13 @@
 import React from "react";
+import LocationCard from "@/components/LocationCard";
+import SubmitButton from "@/components/SubmitButton";
 
 export default function Page() {
     return (
-        <div>
-            <div>registry</div>
+        <div className="flex flex-col lg:items-center font-cormorant text-center">
+            <p className="lg:w-1/3 text-2xl">Our wedding registry is being managed by The Knot. To add to our funds and purchase us a gift please navigate to our registry site by clicking the button below.</p>
+            <div className="py-8 text-2xl ">Thank you so much for the support!</div>
+            <div className="lg:w-1/3 "><a href="https://registry.theknot.com/dillon-rowan-october-2024-tx/63291690"><SubmitButton label={"REGISTRY"} /></a></div>
         </div>
     );
 }
